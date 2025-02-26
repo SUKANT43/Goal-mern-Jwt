2ddbf49a15f9499e695341ea35a9b243f757293f
sukant43 <sukant98657@gmail.com>
2025-02-26 18:31:14 +0530

    db

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -1,19 +1,54 @@
-const goal=require('../model/goalModel')
+const Goal=require('../model/goalModel')
 
-const getGoals=(req,res)=>{
-    res.json({mes:"hi"})
+const getGoals=async(req,res)=>{
+    try{
+    const goals=await Goal.find()
+    res.status(200).json({goals})
+    }
+    catch(err){
+        res.status(400).json(err)
+    }
 }
 
-const postGoals=(req,res)=>{
-    res.json({mes:"hi"})
+const postGoals=async(req,res)=>{
+    try{
+        const goals=await Goal.create({
+            text:req.body.text
+        })
+        res.status(200).json({msg:"goal added"})
+        }
+        catch(err){
+            res.status(400).json(err)
+        }
 }
 
-const putGoals=(req,res)=>{
-    res.json({mes:"hi"+req.params.id})
+const putGoals=async(req,res)=>{
+    try{
+        const goal=await Goal.findByIdAndUpdate(req.params.id,req.body,{
+            new:true
+        })
+        if(!goal){
+            res.status(200).json({msg:"there is no data for this id"})
+        }
+        res.status(200).json(goal)
+    }
+    catch(err){
+        res.status(400).json(err)
+    }
 }
 
-const deleteGoals=(req,res)=>{
-    res.json({mes:"hi"+req.params.id})
+const deleteGoals=async(req,res)=>{
+    try{
+        const goal=await Goal.findByIdAndDelete(req.params.id)
+        if(!goal){
+            res.status(200).json({msg:"there is no data for this id"})
+        }else{
+            res.status(200).json({msg:"data deleted"})
+        }
+    }
+    catch(err){
+        res.status(400).json(err)
+    }
 }
 
 
