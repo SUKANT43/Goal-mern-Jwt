e24bb274bf9f84a93364bcb43d5d25523a73a03c
sukant43 <sukant98657@gmail.com>
2025-02-26 18:02:29 +0530

    db

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,18 @@
 const express=require("express")
+const cors=require('cors')
 const dotenv=require("dotenv").config()
 const port=process.env.PORT||2008;
 const router=require('./routes/goalRoutes')
 const app=express()
+const connectDB=require('./config/db')
+app.use(cors())
+
 app.use(express.json())
+
 app.use('/api',router)
 
 
 app.listen(port,()=>{
     console.log("Port is running in:"+port);
-    
+    connectDB()
 })
