1fbfccf17a2704807fe8479ebbce43cf1f6ed677
sukant43 <sukant98657@gmail.com>
2025-02-27 10:24:06 +0530

    jwt

---
+++ backend/controller/userController.js
@@ -0,0 +1,21 @@
+const jwt=require('jsonwebtoken')
+const bcrypt=require('bcryptjs')
+const User=require('../model/userModel')
+const registerUser=async(req,res)=>{
+        
+}
+
+const loginUser=async(req,res)=>{
+    
+}
+
+
+const getME=async(req,res)=>{
+    
+}
+
+
+
+
+
+module.exports={registerUser,loginUser,getME}
--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,10 @@ const express=require("express")
 const cors=require('cors')
 const dotenv=require("dotenv").config()
 const port=process.env.PORT||2008;
-const router=require('./routes/goalRoutes')
+const goalRouter=require('./routes/goalRoutes')
 const app=express()
 const connectDB=require('./config/db')
-
+const userRouter=require('./routes/userRoutes')
 app.use((req, res, next) => {
     console.log(`[${new Date().toISOString()}] ${req.method} ${req.path}`);
     next();
@@ -16,7 +16,9 @@ app.use(cors())
 
 app.use(express.json())
 
-app.use('/api',router)
+app.use('/api/goal',goalRouter) 
+app.use('/api/user',userRouter)
+
 
 
 app.listen(port,()=>{
